fec0d183be20e26581df3c1dd4f4b1f575a7640a
Obulareddy Veera Bharagava Reddy <Obulareddy.Reddy@yodlee.com>
2020-10-05 14:04:15 +0530

    Integration with API

--- pages/auth_callback.js
+++ pages/auth_callback.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { useRouter } from "next/router";
 
-function OAuthCallback({ Component, pageProps }) {
+function OAuthCallback() {
+  console.log("--=== I am token controller ===--");
+  const router = useRouter();
+  React.useEffect(() => {
+    const search = window.location.search;
+    const params = new URLSearchParams(search);
+    const token = params.get("token");
+    if (token) {
+      window.localStorage.setItem("jwt", token);
+      router.push("/dashboard");
+    }
+  });
+
   return (
-      <div>
-        <div>I am OAuthComponent ....Loading</div>
-      </div>
-  )
+    <div>
+      <div>I am OAuthComponent ....Loading</div>
+    </div>
+  );
 }
 
-export default OAuthCallback
+export default OAuthCallback;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
+import React from "react";
 import Head from "next/head";
+import GoogleButton from "react-google-button";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const fetchOAuth2LoginLink = React.useCallback(() => {
+    console.log("--== fetchOAuth2LoginLink ==--");
+    try {
+      fetch("http://api.farmer.accounts.easyfarm.co.in/")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          window.location.href = data.loginLink;
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
   return (
     <div className={styles.container}>
       <div>I am home page</div>
+      <GoogleButton onClick={() => fetchOAuth2LoginLink()} />
     </div>
   );
 }
