e5787861785579a6ef8f90d57f255ede04ce3c1d
Obulareddy Veera Bharagava Reddy <Obulareddy.Reddy@yodlee.com>
2020-10-05 11:38:17 +0530

    Setting up Next.js

---
+++ pages/auth_callback.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function OAuthCallback({ Component, pageProps }) {
+  return (
+      <div>
+        <div>I am OAuthComponent ....Loading</div>
+      </div>
+  )
+}
+
+export default OAuthCallback
---
+++ pages/dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Dashboard() {
+  return (
+      <div>
+          <div>Dashboard Recieved JWT Token</div>
+          <div>Develop React.js Application</div>
+      </div>
+  );
+
+}
+
+export default Dashboard;
